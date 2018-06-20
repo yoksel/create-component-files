d9c07b68acc6d5c16b3f18fe30ce2135b966fe92
yokselzok <yokselzok@gmail.com>
2018-06-20 15:17:58 +0300

    Add linter, lint code

--- bin/index.js
+++ bin/index.js
@@ -20,140 +20,136 @@ const usage = `${gold('Usage:')} $(npm bin)/create-component-files --path=./src/
 // ------------------------------
 
 function createReactComponent() {
-    // Input arguments
-    const args = process.argv.slice(2);
+  // Input arguments
+  const args = process.argv.slice(2);
 
-    if (args.length === 0) {
-        console.log(usage);
-        return;
+  if (args.length === 0) {
+    console.log(usage);
+    return;
+  }
+
+  const params = args.reduce((obj, item) => {
+    let [name, value] = item.split('=');
+    name = name.replace('--', '');
+
+    if (value) {
+      obj[name] = value;
+    } else {
+      obj['name'] = name;
     }
 
-    const params = args.reduce((obj, item) => {
-        let [name, value] = item.split('=');
-        name = name.replace('--', '');
+    return obj;
+  }, {});
 
-        if (value) {
-            obj[name] = value;
-        }
-        else {
-            obj['name'] = name;
-        }
+  if (!params.name) {
+    console.log(error('Component not created:'), 'pass a component name');
+    console.log(usage);
+    return;
+  }
 
-        return obj;
-    }, {});
+  if (isLowerCase(params.name)) {
+    console.log(error('Component was not created:'));
+    console.log('React component name must starts with an uppercase letter');
+    return;
+  }
 
-    if (!params.name) {
-        console.log(error(`Component not created:`), 'pass a component name');
-        console.log(usage);
-        return;
-    }
+  if (params.path === undefined) {
+    params.path = setCurrentDir(params);
+  } else if (params.path.slice(-1) !== '/') {
+    params.path += '/';
+  }
 
-    if (isLowerCase(params.name)){
-        console.log(error(`Component was not created:`));
-        console.log('React component name must starts with an uppercase letter');
-        return;
-    }
-
-    if (params.path === undefined) {
-        params.path = setCurrentDir(params);
-    }
-    else if(params.path.slice(-1) !== '/') {
-        params.path += '/';
-    }
-
-    createFileTree(params);
+  createFileTree(params);
 }
 
 // Create files & folders
 // ------------------------------
 
 function createFileTree(params) {
-    const jsContent = getJsContent(params);
-    const cssContent = getCSSContent(params);
-    const mdContent = getMDContent(params);
-    let componentPath = `${params.path}${params.name}`;
+  const jsContent = getJsContent(params);
+  const cssContent = getCSSContent(params);
+  const mdContent = getMDContent(params);
+  let componentPath = `${params.path}${params.name}`;
 
-    // Add folder
-    fs.mkdir(componentPath, (err) => {
-        if (err) {
-            if (err.code === 'EEXIST') {
-                console.log(error(`Component was not created:`));
-                console.log(`path ${componentPath} already exists`);
-            }
-            else if (err.code === 'ENOENT') {
-                console.log(error(`Component was not created:`));
-                console.log(`path ${params.path} doesn't exist`);
-            }
-            else {
-                console.log(error(`Component was not created:`));
-                console.log(err);
-            }
+  // Add folder
+  fs.mkdir(componentPath, (err) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.log(error('Component was not created:'));
+        console.log(`path ${componentPath} already exists`);
+      } else if (err.code === 'ENOENT') {
+        console.log(error('Component was not created:'));
+        console.log(`path ${params.path} doesn't exist`);
+      } else {
+        console.log(error('Component was not created:'));
+        console.log(err);
+      }
 
-            return;
-        }
+      return;
+    }
 
-        // Add JS-file
-        fs.writeFile(`${componentPath}/index.js`, jsContent, (err) => {
-            errorMessage(err, 'JS');
+    // Add JS-file
+    fs.writeFile(`${componentPath}/index.js`, jsContent, (jsFileErr) => {
+      errorMessage(jsFileErr, 'JS');
 
-            // Add CSS-file
-            fs.writeFile(`${componentPath}/${params.name}.scss`, cssContent, (err) => {
-                errorMessage(err, 'SCSS');
+      // Add CSS-file
+      fs.writeFile(`${componentPath}/${params.name}.scss`, cssContent, (cssFileErr) => {
+        errorMessage(cssFileErr, 'SCSS');
 
-                // Add md-file
-                fs.writeFile(`${componentPath}/README.md`, mdContent, (err) => {
-                    errorMessage(err, 'README');
+        // Add md-file
+        fs.writeFile(`${componentPath}/README.md`, mdContent, (mdFileErr) => {
+          errorMessage(mdFileErr, 'README');
 
-                    successMessage(componentPath);
-                });
-            });
+          successMessage(componentPath);
         });
-
+      });
     });
+
+  });
 }
 
 // Helpers
 // ------------------------------
 
 function setCurrentDir(params) {
-    const currentDir = path.relative(process.cwd(), process.env.INIT_CWD);
-    let currentPath = '';
+  const currentDir = path.relative(process.cwd(), process.env.INIT_CWD);
+  let currentPath = '';
 
-    if (currentDir) {
-        currentPath = `./${currentDir}/`;
-    }
+  if (currentDir) {
+    currentPath = `./${currentDir}/`;
+  }
 
-    return currentPath;
+  return currentPath;
 }
 
 function isLowerCase(name) {
-    const firstNameLetter = name.substr(0,1);
+  const firstNameLetter = name.substr(0,1);
 
-    return firstNameLetter === firstNameLetter.toLowerCase();
+  return firstNameLetter === firstNameLetter.toLowerCase();
 }
 
 function successMessage(componentPath) {
-    const finalPath = componentPath.replace('././', '');
+  const finalPath = componentPath.replace('././', '');
 
-    console.log(`${success(`Component was created:`)} ${finalPath}`);
+  console.log(`${success('Component was created:')} ${finalPath}`);
 }
 
 function errorMessage(err, fileTypetype) {
-    if(err) {
-        console.log(`${error(`${fileTypetype}-file for component was not created:`)}`);
-        console.log(err);
-    }
+  if (err) {
+    console.log(`${error(`${fileTypetype}-file for component was not created:`)}`);
+    console.log(err);
+  }
 }
 
 function getJsContent(params) {
-return `import React, { Component } from 'react';
+  return `import React, { Component } from 'react';
 import './${params.name}.css';
 
 class ${params.name} extends Component {
   render() {
-    return (
-        <div className="${params.name}">${params.name}</div>
-    );
+  return (
+    <div className="${params.name}">${params.name}</div>
+  );
   }
 }
 
@@ -165,13 +161,13 @@ function getCSSContent(params) {
   const randomPos = Math.floor(Math.random() * colors.length);
   const color = colors[randomPos];
 
-return `.${params.name} {
-    color: ${color};
+  return `.${params.name} {
+  color: ${color};
 }`;
 }
 
 function getMDContent(params) {
-return `# ${params.name}`;
+  return `# ${params.name}`;
 }
 
 // ------------------------------
